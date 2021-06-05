73490d0b03c5c72ce1c416944425c8eec2de9a29
rubyneko <84444784+rubyneko@users.noreply.github.com>
2021-06-05 12:13:20 +0900

    Create Ex2_1.java

---
+++ Ex2_1.java
@@ -0,0 +1,10 @@
+class Ex2_1 { 
+	public static void main(String args[]) { 
+		//alt+shift+a 는 컬럼편집 모드(토글)
+		//ctrl+space 자동완성
+		//ctrl+alt+shift+down 행단위복사
+		//println() - 출력 후에 줄바꿈
+		System.out.println(3+5); //덧셈
+		System.out.println(5-3); //뺄셈
+		System.out.println(5*3); //곱셈
+		System.out.println(5/3.0); //나눗셈
